3600e999d7546481c36814c0bd5c2c19a1fba897
Ian Scalzo <ian@current.si>
2019-08-15 15:10:37 -0400

    added dist

--- express/app.js
+++ express/app.js
@@ -8,14 +8,14 @@ const serverless = require('serverless-http');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', path.join(__dirname, '../dist/views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../dist/public')));
 
 const router = require('./routes');
 
