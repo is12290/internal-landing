97d3a86058dca2e87c6e1535851794529a3ac12d
Ian Scalzo <ian@current.si>
2019-08-15 15:02:31 -0400

    All ready for netlify.... I think

---
+++ express/app.js
@@ -0,0 +1,31 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const serverless = require('serverless-http');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
+
+const router = require('./routes');
+
+app.use('/.netlify/functions/app', router)
+
+// app.use(router);
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+module.exports = app;
+module.exports.handler = serverless(app);
---
+++ express/routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { page: 'Home', menuId: 'home' });
+});
+
+router.get('/privacy', function(req, res, next) {
+  res.render('privacy', { page: 'privacy', menuId: 'privacy' });
+});
+
+router.get('/data-processing-addendum', function(req, res, next) {
+  res.render('data-processing-addendum', { page: 'data-processing-addendum', menuId: 'data-processing-addendum' });
+});
+
+router.get('/terms', function(req, res, next) {
+  res.render('terms', { page: 'terms', menuId: 'terms' });
+});
+
+router.get('/success-checkout', function(req, res, next) {
+  res.render('success-checkout', { page: 'success-checkout', menuId: 'success-checkout' });
+});
+
+router.get('/error-checkout', function(req, res, next) {
+  res.render('error-checkout', { page: 'error-checkout', menuId: 'error-checkout' });
+});
+
+router.get('/sike', function(req, res, next) {
+  res.render('sike', { page: 'sike', menuId: 'sike' });
+});
+
+router.get('/whoops', function(req, res, next) {
+  res.render('whoops', { page: 'whoops', menuId: 'whoops' });
+});
+
+
+module.exports = router;
